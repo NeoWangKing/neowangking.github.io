16aed80c7b6043c0f76e42769e4985dfc31dabd0
NeoWangKing <NeoWangKing@users.noreply.github.com>
2026-05-04 08:05:47 +0000

    deploy: NeoWangKing/blog-source-code@9ae85886ce530cf13003493ad4a1d9b6e2a1694c

---
+++ _astro/neovin.c.CFtuFP8M.js
@@ -0,0 +1,5 @@
+const n="neovin.c",t="projects",e={title:"neovin.c",description:"一个用C语言开发的纯头文件的图形库",image:"https://s2.loli.net/2024/03/02/hba5MAilRXguZtr.webp",link:"https://github.com/NeoWangKing/neovin.c"},o={type:"data",filePath:"/home/runner/work/blog-source-code/blog-source-code/src/content/projects/neovin.c.yml",rawData:`title: neovin.c
+description: 一个用C语言开发的纯头文件的图形库
+image: https://s2.loli.net/2024/03/02/hba5MAilRXguZtr.webp
+link: https://github.com/NeoWangKing/neovin.c
+`};export{o as _internal,t as collection,e as data,n as id};
